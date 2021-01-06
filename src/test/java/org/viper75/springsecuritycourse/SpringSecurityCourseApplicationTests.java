9cf5497c73055ed6ccfe9227f665e751f1dce26a
viper75 <sean.huvaya03@yahoo.com>
2021-01-06 18:00:25 +0200

    Initial commit

---
+++ src/test/java/org/viper75/springsecuritycourse/SpringSecurityCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.viper75.springsecuritycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
